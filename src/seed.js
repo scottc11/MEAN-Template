ab14f4ec3fb9514825866bf227cbf3875080e167
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2017-02-17 21:44:08 -0600

    cleaned up seed.js file

--- src/seed.js
+++ src/seed.js
@@ -1,8 +1,7 @@
 'use strict';
 
 var User = require('./models/userSchema.js');
-var Group = require('./models/groupSchema.js');
+
 
 // some data to get things rolling
 var seedData = require('../mock/seedData.json');
-var groupSeedData = require('../mock/groupSeedData.json');
